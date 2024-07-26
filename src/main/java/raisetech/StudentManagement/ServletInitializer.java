4257cb62fe9e53b932037e6439d8542aba53a90f
lengjie07 <rm0730mr.shbc@gmail.com>
2024-07-26 23:39:35 +0900

    指摘箇所の修正

--- src/main/java/raisetech/StudentManagement/ServletInitializer.java
+++ src/main/java/raisetech/StudentManagement/ServletInitializer.java
@@ -1,4 +1,4 @@
-package raisetech.StudentManagement;
+package raisetech.studentmanagement;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;

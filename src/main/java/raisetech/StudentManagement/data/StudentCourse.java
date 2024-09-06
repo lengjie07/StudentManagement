7386e161e91b7f14f79d634711eba9402f5ffe08
lengjie07 <rm0730mr.shbc@gmail.com>
2024-09-06 15:41:09 +0900

    コース情報を一緒に登録する処理を実装

--- src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -1,6 +1,7 @@
 package raisetech.studentmanagement.data;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,6 @@ public class StudentCourse {
   private int id;
   private int studentId;
   private String courseName;
-  private Timestamp startDate;
-  private Timestamp endDate;
+  private LocalDateTime startDate;
+  private LocalDateTime endDate;
 }
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -3,12 +3,13 @@ package raisetech.studentmanagement.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
 
 @Mapper
-public interface StudentRepsitory {
+public interface StudentRepository {
 
   @Select("SELECT * FROM students")
   List<Student> searchStudent();
@@ -18,5 +19,10 @@ public interface StudentRepsitory {
 
   @Insert("INSERT INTO students (full_name, furigana, nickname, email_address, area, age, gender, remark, is_deleted) " +
       "VALUES (#{fullName}, #{furigana}, #{nickname}, #{emailAddress}, #{area}, #{age}, #{gender}, #{remark}, #{isDeleted})")
+  @Options(useGeneratedKeys = true, keyProperty = "id") // 自動生成されたIDを取得
   void insertStudent(Student student);
+
+  @Insert("INSERT INTO students_courses (student_id, course_name, start_date, end_date) " +
+      "VALUES (#{studentId}, #{courseName}, #{startDate}, #{endDate})")
+  void insertStudentCourse(StudentCourse studentCourse);
 }

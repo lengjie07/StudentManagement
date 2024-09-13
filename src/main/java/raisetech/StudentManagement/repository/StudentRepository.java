a67fe8e4fb0ece42ece1e759a359c5001467cd19
lengjie07 <rm0730mr.shbc@gmail.com>
2024-09-13 21:51:01 +0900

    34_Create処理のREST化とリファクタリング

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -9,15 +9,47 @@ import org.apache.ibatis.annotations.Update;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
 
+/**
+ * 受講生テーブルとコース情報テーブルと紐づくリポジトリ
+ */
 @Mapper
 public interface StudentRepository {
 
+  /**
+   * 受講生情報の全件検索
+   * @return 受講生情報リスト
+   */
   @Select("SELECT * FROM students")
   List<Student> searchStudent();
 
+  /**
+   * コース情報の全件検索
+   * @return コース情報リスト
+   */
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourse();
 
+  /**
+   * IDで指定した受講生情報の検索
+   * @param id　受講生ID
+   * @return IDで指定した受講生情報
+   */
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student findStudentById(int id);
+
+  /**
+   * 受講生IDで指定した受講生のコース情報
+   * @param studentId 受講生ID
+   * @return 受講生IDで指定した受講生のコース情報リスト
+   */
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentCourse> findStudentCoursesByStudentId(int studentId);
+
+  /**
+   * 新規受講生情報の登録
+   * 受講生情報を登録する際にコース情報も一緒に行う
+   * @param student 受講生情報
+   */
   @Insert("""
           INSERT INTO students (full_name, furigana, nickname, email_address, area, age, gender, remark, is_deleted)
           VALUES (#{fullName}, #{furigana}, #{nickname}, #{emailAddress}, #{area}, #{age}, #{gender}, #{remark}, false)
@@ -26,18 +58,21 @@ public interface StudentRepository {
     // 自動生成されたIDを取得
   void insertStudent(Student student);
 
+  /**
+   * 新規コース情報の登録
+   * コース情報の登録は受講生情報の登録と一緒に行う
+   * @param studentCourse コース情報
+   */
   @Insert("""
           INSERT INTO students_courses (student_id, course_name, start_date, end_date)
           VALUES (#{studentId}, #{courseName}, #{startDate}, #{endDate})
           """)
   void insertStudentCourse(StudentCourse studentCourse);
 
-  @Select("SELECT * FROM students WHERE id = #{id}")
-  Student findStudentById(int id);
-
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentCourse> findStudentCoursesByStudentId(int studentId);
-
+  /**
+   * 受講生情報の更新
+   * @param student 受講生情報
+   */
   @Update("""
       UPDATE students
       SET
@@ -54,7 +89,11 @@ public interface StudentRepository {
       """)
   void updateStudent(Student student);
 
-  // コース名、開始日、終了日がnullの場合は更新しないようにする
+  /**
+   * コース情報の更新
+   * コース名、開始日、終了日がnullの場合は更新しないようにする
+   * @param studentCourse コース情報
+   */
   @Update("""
       <script>
         UPDATE students_courses

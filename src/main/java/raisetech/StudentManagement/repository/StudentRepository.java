ca760c824412fbac242eaf2b7d625dcb3af5489e
lengjie07 <rm0730mr.shbc@gmail.com>
2024-09-14 16:56:36 +0900

    StudentRepositoryをxmlファイルに変更

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,11 +1,7 @@
 package raisetech.studentmanagement.repository;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
 
@@ -25,7 +21,6 @@ public interface StudentRepository {
    * コース情報の全件検索
    * @return コース情報リスト
    */
-  @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourse();
 
   /**
@@ -33,7 +28,6 @@ public interface StudentRepository {
    * @param id　受講生ID
    * @return IDで指定した受講生情報
    */
-  @Select("SELECT * FROM students WHERE id = #{id}")
   Student findStudentById(int id);
 
   /**
@@ -41,7 +35,6 @@ public interface StudentRepository {
    * @param studentId 受講生ID
    * @return 受講生IDで指定した受講生のコース情報リスト
    */
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> findStudentCoursesByStudentId(int studentId);
 
   /**
@@ -50,12 +43,6 @@ public interface StudentRepository {
    * 受講生情報を登録する際にコース情報も一緒に行う
    * @param student 受講生情報
    */
-  @Insert("""
-          INSERT INTO students (full_name, furigana, nickname, email_address, area, age, gender, remark, is_deleted)
-          VALUES (#{fullName}, #{furigana}, #{nickname}, #{emailAddress}, #{area}, #{age}, #{gender}, #{remark}, false)
-          """)
-  @Options(useGeneratedKeys = true, keyProperty = "id")
-    // 自動生成されたIDを取得
   void insertStudent(Student student);
 
   /**
@@ -64,30 +51,12 @@ public interface StudentRepository {
    * コース情報の登録は受講生情報の登録と一緒に行う
    * @param studentCourse コース情報
    */
-  @Insert("""
-          INSERT INTO students_courses (student_id, course_name, start_date, end_date)
-          VALUES (#{studentId}, #{courseName}, #{startDate}, #{endDate})
-          """)
   void insertStudentCourse(StudentCourse studentCourse);
 
   /**
    * 受講生情報の更新
    * @param student 受講生情報
    */
-  @Update("""
-      UPDATE students
-      SET
-        full_name = #{fullName},
-        furigana = #{furigana},
-        nickname = #{nickname},
-        email_address = #{emailAddress},
-        area = #{area},
-        age = #{age},
-        gender = #{gender},
-        remark = #{remark},
-        is_deleted = #{isDeleted}
-      WHERE id = #{id}
-      """)
   void updateStudent(Student student);
 
   /**
@@ -95,22 +64,5 @@ public interface StudentRepository {
    * コース名、開始日、終了日がnullの場合は更新しないようにする
    * @param studentCourse コース情報
    */
-  @Update("""
-      <script>
-        UPDATE students_courses
-        <set>
-          <if test='courseName != null'>
-            course_name = #{courseName},
-          </if>
-          <if test='startDate != null'>
-            start_date = #{startDate},
-          </if>
-          <if test='endDate != null'>
-            end_date = #{endDate},
-          </if>
-        </set>
-        WHERE id = #{id} AND student_id = #{studentId}
-      </script>
-      """)
   void updateStudentCourse(StudentCourse studentCourse);
 }

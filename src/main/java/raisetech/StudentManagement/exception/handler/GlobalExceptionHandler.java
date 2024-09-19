5e035979ab890bb0bb655f8e5d667087cabb2309
lengjie07 <rm0730mr.shbc@gmail.com>
2024-09-19 23:00:25 +0900

    Post,Put処理の入力チェックでBAD_REQUESTが発生した際の例外

--- src/main/java/raisetech/StudentManagement/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/raisetech/StudentManagement/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package raisetech.studentmanagement.exception.handler;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import raisetech.studentmanagement.exception.StudentNotFoundException;
@@ -17,5 +21,21 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(StudentNotFoundException.class)
   public ResponseEntity<String> handleStudentNotFoundException(StudentNotFoundException ex){
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  /**
+   * Post,Put処理の入力チェックでBAD_REQUESTが発生した際の例外をハンドリングする
+   * @param ex handleMethodArgumentNotValidException
+   * @return カラム名とデフォルトのエラーメッセージ
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+    Map<String,String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }

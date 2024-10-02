211f35cb8f3ee2921ed9d050bf6587941fc2bb8e
lengjie07 <rm0730mr.shbc@gmail.com>
2024-10-02 16:35:39 +0900

    テストコード用のコンストラクタを作成したら、POSTとPUTが動作しなくなったので、@NoArgsConstructorを追加

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Schema(description = "受講生情報")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Student {
 
   private int id;
